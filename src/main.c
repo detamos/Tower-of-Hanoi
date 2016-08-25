b338f44dca17a8a9fdaf3509ffd41514e829243c
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2016-08-25 15:21:36 +0530

    comments are added

--- src/main.c
+++ src/main.c
@@ -3,22 +3,25 @@
 #include <string.h>
 #include <time.h>
 
-typedef long long int lli;
-
 enum task{recur,iter,both};
 enum bool{true,false};
 
+//function declarations of recursive and iterative functions from their respective c files
 void recursive(int numOfDisks);
 void iterative(int numOfDisks);
 
 int main(int argc,char *argv[])
 {
-	system("clear\n");
+	system("clear\n"); // clearing the terminal screen for clear output
+
+	FILE *log = NULL;
+	log = fopen("log","w"); // opening log file for easy debugging
 
 	int numbOfDisks;
 	enum bool input;
 	enum task do_task;
 
+	//checking the number and types of arguments 
 	switch(argc)
 	{
 	case 1:	input = false;
@@ -72,33 +75,51 @@ int main(int argc,char *argv[])
 			do_task = both;
 			input = false;
 	}
-
+	// if input is still false then value of numbOfDisks has to be taken from user
 	if(input == false)
 	{
 		printf("Enter the number of disks :\t");
 		scanf("%d",&numbOfDisks);
 	}
 
-	//clock_t start,end;
+	// sending logs to file log
+	fprintf(log,"All the variables are declared\nNumber of arguments %d\n",argc);
+	fprintf(log,"The values of enums are :\ninput :\t%d\ndo_task :\t%d\n",input,do_task);
+	fprintf(log,"The number of disks are as follows : \t%d\n\n",numbOfDisks);
 
+	clock_t start,end;
+
+	//checking how the user wants to do the program ,using recursive or iterative or both 
 	switch(do_task)
 	{
-	case iter:	//start = clock();
+				//in case of do_task = iter ,iterative method is used
+	case iter:	fprintf(log,"Sending handle to iterative function...\n");
+				start = clock();
 				iterative(numbOfDisks);
-				//end = clock();
+				end = clock();
+				fprintf(log,"Tower of hanoi solved by iterative approach in %lf sec\n\n",(double)(end-start)/CLOCKS_PER_SEC);
 				break;
-	case recur:	//start = clock();
+				//in case of do_task = recur ,recursive method is used
+	case recur:	fprintf(log,"Sending handle to recursive function...\n");
+				start = clock();
 				recursive(numbOfDisks);
-				//end = clock();
+				end = clock();
+				fprintf(log,"Tower of hanoi solved by recursive approach in %lf sec\n\n",(double)(end-start)/CLOCKS_PER_SEC);
 				break;
-	case both:	//start = clock();
+				//in case of do_task = both, both the methods are used
+	case both:	fprintf(log,"Sending handle to recursive function...\n");
+				start = clock();
 				recursive(numbOfDisks);
-				//end = clock();
+				end = clock();
+				fprintf(log,"Tower of hanoi solved by recursive approach in %lf sec\n\n",(double)(end-start)/CLOCKS_PER_SEC);
 
-				//start = clock();
+				fprintf(log,"Sending handle to iterative function...\n");
+				start = clock();
 				iterative(numbOfDisks);
-				//end = clock();
+				end = clock();
+				fprintf(log,"Tower of hanoi solved by iterative approach in %lf sec\n\n",(double)(end-start)/CLOCKS_PER_SEC);
 				break;
 	}
+	fclose(log);
 	return 0;
 }
